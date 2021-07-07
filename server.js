7c13d3c30074dbd279a2a2689f4262bc02d84c56
Rodrigo Tufino <rtufino@ups.edu.ec>
2021-07-07 12:59:11 -0500

    Agregar un usuario

--- server.js
+++ server.js
@@ -1,37 +1,23 @@
 require("./server/config");
 
 const express = require('express');
+const mongoose = require('mongoose');
+
 const app = express();
 
 
 app.use(express.urlencoded({ extended: false }));
 
-app.get('/usuario', function(req, res) {
-    res.json('get Usuario');
-});
+app.use(require('./server/routes/usuario'));
 
-app.put('/usuario', function(req, res) {
-    let body = req.body;
 
-    if (body.nombre === undefined) {
-        res.status(400).json({
-            ok: false,
-            mensaje: "El nombre es requerido"
-        })
-    } else {
-        res.json({
-            persona: body
-        });
-    }
-});
+mongoose.connect(
+    'mongodb://localhost:27017/Cafe', { useCreateIndex: true, useNewUrlParser: true, useUnifiedTopology: true },
+    (err, res) => {
+        if (err) throw err;
 
-app.post('/usuario', function(req, res) {
-    res.json('post Usuario');
-});
-
-app.delete('/usuario', function(req, res) {
-    res.json('delete Usuario');
-});
+        console.log("Base de datos ONLINE!");
+    });
 
 app.listen(process.env.PORT, () => {
     console.log("Escuchando en el puerto:", process.env.PORT);
