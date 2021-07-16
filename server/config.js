713eb0278ac364d447f0e3be3dbe33102470e498
Rodrigo Tufino <rtufino@ups.edu.ec>
2021-07-16 11:16:57 -0500

    Configuración de la BDD

--- server/config.js
+++ server/config.js
@@ -3,3 +3,23 @@
 //-------------------------------
 
 process.env.PORT = process.env.PORT || 3000;
+
+// -----------------------------
+// Entorno
+//-------------------------------
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+// -----------------------------
+// Base de datos
+//-------------------------------
+
+let urlDB;
+
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/Cafe'
+} else {
+    urlDB = 'mongodb+srv://bree:jZPPpPi9QVE6sLAW@aravis.wbqas.mongodb.net/Cafe?retryWrites=true&w=majority'
+}
+
+process.env.URLDB = urlDB;
